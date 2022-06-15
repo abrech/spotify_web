07005d8c2d7d5dadca23e2211203adbb81b606cf
abrech <adrian.brechtken@gmail.com>
2022-06-15 13:27:14 +0200

    test

--- static/index.js
+++ static/index.js
@@ -1,5 +1,8 @@
 function test(){
-    fetch('https:///172.18.0.6:6500/test').then(data=>data.json()).then(data=>console.log(data));
+    fetch('https:///172.18.0.6:6500/test').then(data=>data.json()).then(data=>{console.log(data); console.log('huh')});
+    setTimeout(() => {
+        test();
+    }, 3000);
 }
 
 test()
