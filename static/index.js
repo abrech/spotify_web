d5ef9d40436ebd5a423aefa28b391bb0a4a25baf
abrech <adrian.brechtken@gmail.com>
2022-06-15 13:22:50 +0200

    host

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 function test(){
-    fetch('https://spotify-data:6400/test').then(data=>data.json()).then(data=>console.log(data));
+    fetch('https:///172.18.0.6:6500/test').then(data=>data.json()).then(data=>console.log(data));
 }
 
 test()
