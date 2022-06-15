8c0ff6f403d628037f684c46480f8f36638996eb
abrech <adrian.brechtken@gmail.com>
2022-06-15 13:31:51 +0200

    ...

--- static/index.js
+++ static/index.js
@@ -1,8 +1,9 @@
 function test(){
-    fetch('https:///172.18.0.6:6500/test').then(data=>data.json()).then(data=>{console.log(data); console.log('huh')});
+    fetch('spotify-data/test').then(data=>data.json()).then(data=>{console.log(data); console.log('huh')});
     setTimeout(() => {
         test();
     }, 3000);
 }
 
-test()
+console.log('hä');
+test();
